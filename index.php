c16e50111c20265c2aa32d9b4b76ba7556ec8656
MenendezON <menendezon@gmail.com>
2021-01-07 22:07:43 -0500

    Update index
    Add firebase script

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Index</title>
+</head>
+<body>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDblZ04Xy8PViipC19rbMWByLdL1m_rdOI",
+    authDomain: "sirh-2020.firebaseapp.com",
+    projectId: "sirh-2020",
+    storageBucket: "sirh-2020.appspot.com",
+    messagingSenderId: "155759661331",
+    appId: "1:155759661331:web:dcc8bb09cc61680c55cae6",
+    measurementId: "G-8P8M29JGKG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+</body>
+</html>
